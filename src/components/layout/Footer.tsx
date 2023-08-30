629773b27a52b9609eb2b72d9eb12a1ba8aa1515
Cannon Lock <clock@wisc.edu>
2023-08-30 15:03:13 -0500

    Add the award to the footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,11 +1,12 @@
 'use client'
 
 import ExportedImage from "next-image-export-optimizer";
-import {Grid, Box, Container, Divider} from "@mui/material";
+import {Grid, Box, Container, Divider, Typography} from "@mui/material";
 
 import chtcLogo from "../../public/static/images/CHTC_Logo.svg"
 import morgridgeLogo from "../../public/static/images/Morgridge_Logo.png"
 import Link from "next/link";
+import {TypeSpecimen} from "@mui/icons-material";
 
 const Footer = () => {
 
@@ -31,7 +32,26 @@ const Footer = () => {
                 <Container maxWidth={"xl"}>
                     <Grid container justifyContent={"center"}>
                         <Grid item>
-                            <Link href={"./branding"}>Branding</Link>
+                            <Typography>
+                                <Link href={"./branding"}>Branding</Link>
+                            </Typography>
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Box>
+            <Box sx={{backgroundColor:"#F5F5F5"}} py={1}>
+                <Container maxWidth={"xl"}>
+                    <Grid container justifyContent={"center"}>
+                        <Grid xs={12} lg={10} item>
+                            <Box>
+                                <Typography variant={"subtitle1"} sx={{textAlign: "center", color: "grey"}}>
+                                    This project is supported by National Science Foundation under Cooperative
+                                    Agreement
+                                    <Link style={{color: "#0885ff"}} href={"https://www.nsf.gov/awardsearch/showAward?AWD_ID=2331480"}> OAC-2331480</Link>. Any opinions, findings, conclusions or recommendations
+                                    expressed in this material are those of the authors and do not necessarily
+                                    reflect the views of the National Science Foundation.
+                                </Typography>
+                            </Box>
                         </Grid>
                     </Grid>
                 </Container>
