52b97e1be9746ce02fc7741c090229502e0b113a
Alperen <bakirci.alperen@gmail.com>
2023-11-17 09:23:26 -0600

    Added About to the nav and added dropdown under it for Whats Pelican and Contact

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -2,8 +2,8 @@
 
 import ExportedImage from "next-image-export-optimizer";
 import githubMark from "../../public/static/images/github-mark.png"
-import {useState, useEffect} from "react";
-import {Grid, Box, Container, Divider} from "@mui/material";
+import {useState, useEffect, useRef} from "react";
+import {Grid, Box, Container, Divider, Menu, MenuItem, Button, Popper, Grow, Paper, ClickAwayListener, MenuList} from "@mui/material";
 import styles from "../../app/page.module.css"
 import {Poppins} from "next/font/google";
 import Link from "next/link";
@@ -22,6 +22,15 @@ export const Header = () => {
 		});
 	}, [])
 
+	const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+	const open = Boolean(anchorEl);
+	const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+	  setAnchorEl(event.currentTarget);
+	};
+	const handleClose = () => {
+	  setAnchorEl(null);
+	};
+
 	return (
 		<div className={`${styles.header} ${scrolledTop ? styles.headerScrolled : ""}`} style={{
 			display: "flex",
@@ -39,6 +48,26 @@ export const Header = () => {
 						<Typography variant={"h5"} pl={1} my={"auto"}>Pelican Platform</Typography>
 					</Box>
 				</Link>
+				<Link href={"/"} style={{display: "flex"}}
+					id="about-button"
+					aria-controls={open ? 'about-menu' : undefined}
+					aria-haspopup="true"
+					aria-expanded={open ? 'true' : undefined}
+				>
+					<Typography my={"auto"} pl={2} lineHeight={1} variant={"h6"} onClick={handleClick}>About</Typography>
+				</Link>
+				<Menu
+					id="about-menu"
+					anchorEl={anchorEl}
+					open={open}
+					onClose={handleClose}
+					MenuListProps={{
+					'aria-labelledby': 'about-button',
+					}}
+				>
+					<MenuItem onClick={handleClose}>Whats Pelican?</MenuItem>
+					<MenuItem onClick={handleClose}>Contact</MenuItem>
+				</Menu>
 				<Link href={"/team"} style={{display: "flex"}}>
 					<Typography my={"auto"} pl={2} lineHeight={1} variant={"h6"}>Team</Typography>
 				</Link>
