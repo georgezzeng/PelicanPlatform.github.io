eac57bc1d0cecbe5c3887573a0da9b8475bd9088
Alperen <bakirci.alperen@gmail.com>
2024-04-12 16:51:56 -0500

    Staff Submodule Integration Draft

---
+++ src/utils/loadStaffData.ts
@@ -0,0 +1,16 @@
+import fs from 'fs';
+import path from 'path';
+import yaml from 'js-yaml';
+import { Member } from '../utils/types';
+
+export const loadStaffData = (): Member[] => {
+  const directory = path.join(process.cwd(), 'staff-list');
+  const filenames = fs.readdirSync(directory).filter(file => file.endsWith('.yml'));
+  const staff: Member[] = filenames.map(filename => {
+    const filePath = path.join(directory, filename);
+    const fileContents = fs.readFileSync(filePath, 'utf8');
+    return yaml.load(fileContents) as Member;
+  });
+  console.log(staff);
+  return staff;
+};
